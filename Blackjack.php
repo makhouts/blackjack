c195f3073dc27c001e7f99c06642ad8e2b9ac408
Sofiane Makhout <makhouts@gmail.com>
2021-10-28 15:59:19 +0200

    init, reached step7

---
+++ Blackjack.php
@@ -0,0 +1,21 @@
+<?php
+
+class Blackjack {
+    private $player;
+    private $dealer;
+    private $deck;
+
+    public function getPlayer() {
+        return $this->player;
+        $this->player = new Blackjack();
+    }
+
+    public function getDealer() {
+        return $this->dealer;
+        $this->dealer = new Blackjack();
+    }
+
+    public function getDeck() {
+        return $this->deck;
+    }
+};
---
+++ Player.php
@@ -0,0 +1,23 @@
+<?php
+
+class Player {
+    private $cards = [];
+    private $lost = false;
+
+    public function hit() {
+
+    }
+
+    public function surrender() {
+
+    }
+
+    public function getScore() {
+
+    }
+
+    public function hasLost() {
+
+    }
+
+};
