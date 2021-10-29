9f8377a10cc324c92973bfdeb7a88cb5c0c59bad
Sofiane Makhout <makhouts@gmail.com>
2021-10-29 14:17:23 +0200

    reached step 9

--- Player.php
+++ Player.php
@@ -4,6 +4,12 @@ class Player {
     private $cards = [];
     private $lost = false;
 
+    function __construct(Deck $deck) {
+        for($i = 0; $i < 2; $i++ ) {
+            $this->cards[] = $deck->drawCard();
+        }
+    }
+
     public function hit() {
 
     }
